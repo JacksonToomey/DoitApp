79236fb130c6747c4442dfbe8ceec2b363cb4e17
Jackson Toomey <jacksontoomey@gmail.com>
2018-10-28 19:30:35 -0400

    Initial Commit

---
+++ DoitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoitApp
+//
+//  Created by Jackson Toomey on 10/28/18.
+//  Copyright © 2018 Jackson Toomey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
